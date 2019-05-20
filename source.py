57f3d7af65bb579e54fc32c56fd345d1e01d5f91
Guilherme Rodrigues <guilherme.r1307@gmail.com>
2019-05-20 15:07:57 -0300

    commit buttons

---
+++ source.py
@@ -0,0 +1,7 @@
+from tkinter import *
+import dictlol as d
+
+def open_details():
+	detail = Tk()
+	detail.geometry("300x300+120+120")
+	detail.mainloop()
--- wiki.py
+++ wiki.py
@@ -1,7 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
+import dictlol as d
+import source as s
 
 ##Variaveis
+cpimg = []
 
 ##JANELA
 root = Tk()
@@ -9,7 +12,7 @@ root.title('League Wiki')
 root.resizable(False, False)
 
 ##FUNDO 
-filebg = PhotoImage(file = "background.png")
+filebg = PhotoImage(file = "background1.png")
 w = filebg.width()
 h = filebg.height()
 root.geometry("%dx%d+50+30" % (w,h)) #JANELA
@@ -18,8 +21,38 @@ canvas = Canvas(height = h, width = w)
 canvas.pack(side='top', fill='both', expand='yes')
 canvas.create_image(0, 0, image=filebg, anchor='nw')
 
-for x in range(4):
-	bt = Button(canvas, width=20, text="Selecionar", fg="white", bg = "#004655")
-	bt.pack(side = 'right', padx=10, pady=5, anchor ='sw')
 
+
+##ARRAY IMAGENS
+for x in range(len(d.champimage)):
+       imgtemp = PhotoImage(file = d.champimage[x])
+       cpimg.append(imgtemp)
+
+##DRAW BUTTONS
+for x in range(7):
+       bt = Button(canvas, image = cpimg[x], width = 118, height = 118, bd = 0, highlightthicknes=0, command=s.open_details)
+       bt.grid(row = 0, column = x, padx=13, pady=18)
+
+for x in range(4):
+       if x < 2:
+              bt = Button(canvas, image = cpimg[x+7], width = 118, height = 118, bd = 0, highlightthicknes=0, command=s.open_details)
+              bt.grid(row = 1, column = x, padx=13, pady=10)
+       else:
+              bt = Button(canvas, image = cpimg[x+7], width = 118, height = 118, bd = 0, highlightthicknes=0, command=s.open_details)
+              bt.grid(row = 1, column = x+3, padx=13, pady=10)
+
+for x in range(4):
+       if x < 2:
+              bt = Button(canvas, image = cpimg[x+11], width = 118, height = 118, bd = 0, highlightthicknes=0, command=s.open_details)
+              bt.grid(row = 2, column = x, padx=13, pady=10)
+       else:
+              bt = Button(canvas, image = cpimg[x+11], width = 118, height = 118, bd = 0, highlightthicknes=0, command=s.open_details)
+              bt.grid(row = 2, column = x+3, padx=13, pady=10)
+
+for x in range(7):
+       bt = Button(canvas, image = cpimg[x+15], width = 118, height = 118, bd = 0, highlightthicknes=0, command=s.open_details)
+       bt.grid(row = 3, column = x, padx=13, pady=10)
+
+
+##MAIN
 root.mainloop()
